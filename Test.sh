5146c694b5ce999f1b5f298a4c1d91054704058d
ECE ENGINEERING <ozgur.eralp@outlook.com>
2015-01-23 15:11:06 +0000

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+echo "-------------------------------------------------------------------"
+echo " THIS IS A TEST APPLICATION IMPLEMENTED FOR MAKEITSMART MODULE " 
+echo " "
+echo " To quit the program press Q"
+echo " To publish a message:"
+echo "      - press P and type your message" 
+echo "      - press ENTER!"
+echo "-------------------------------------------------------------------"
+
+sudo ./armTestApp
